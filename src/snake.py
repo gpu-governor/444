582877c43f46f219490c9d00b8278ee519a05074
gpu governor <gpu.governor@gmail.com>
2025-01-08 01:11:22 +0100

    still working  in snake game

--- src/snake.py
+++ src/snake.py
@@ -20,6 +20,15 @@ class Snake:
         self.head = pygame.Rect(self.x,self.y,BLOCK_SIZE,BLOCK_SIZE)
         self.body = [pygame.Rect(self.x-BLOCK_SIZE,self.y,BLOCK_SIZE,BLOCK_SIZE)]
         self.dead = False
+
+    def update(self):
+        self.body.append(self.head)
+        for i in range (len(self.body)-1):
+            self.body[i].x, self.body[i].y = self.body[i+1].x, self.body[i+1].y
+        self.head.x += self.xdir * BLOCK_SIZE
+        self.head.y += self.ydir * BLOCK_SIZE
+        self.body.remove(self.head)
+
 def drawGrid():
     for x in range(0,SW, BLOCK_SIZE):
         for y in range(0,SH, BLOCK_SIZE):
@@ -33,7 +42,22 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        if event.type == pygame.KEYDOWN:
+            if event.key==pygame.K_DOWN:
+                snake.ydir=1  
+                snake.xdir=0  
+            elif event.key==pygame.K_UP:
+                snake.ydir=-1  
+                snake.xdir=0  
+            if event.key==pygame.K_RIGHT:
+                snake.ydir=0  
+                snake.xdir=1  
+            if event.key==pygame.K_LEFT:
+                snake.ydir=0  
+                snake.xdir=-1  
+    snake.update()
+    screen.fill("black")
+    drawGrid()
     pygame.draw.rect(screen,"green",snake.head)
     for square in snake.body:
         pygame.draw.rect(screen,"green",square)
